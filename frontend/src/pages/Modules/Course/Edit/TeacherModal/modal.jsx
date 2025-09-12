9c59e7c6d8fdc6d0b95f30945aaf2af45d888a2d
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2025-09-12 01:38:18 +0100

    feat: ✨ add List Filtering

--- frontend/src/pages/Modules/Course/Edit/TeacherModal/modal.jsx
+++ frontend/src/pages/Modules/Course/Edit/TeacherModal/modal.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Alert, CheckBox, PrimaryButton, SecundaryButton } from "../../../../../components";
+import { useDebounce } from "../../../../../utils";
 
 const CommissionModal = ({show, setShow, area, teachers, comission, setComission}) => {
 
@@ -24,6 +25,41 @@ const CommissionModal = ({show, setShow, area, teachers, comission, setComission
     });
   };
 
+
+	const [name, setName] = useState(null);
+	const [email, setEmail] = useState(null);
+
+	const debouncedName = useDebounce(name, 300);
+	const debouncedEmail = useDebounce(email, 300);
+
+	useEffect(() => {
+		updateFilter('name', debouncedName);
+		updateFilter('email', debouncedEmail);
+	}, [debouncedName, debouncedEmail]);
+
+	const [filters, setFilters] = useState({
+		active: false,
+		name: null,
+		email: null,
+	});
+	const updateFilter = (key, value) => {
+		setFilters(prev => ({
+			...prev,
+			[key]: value === 'all' ? null : value
+		}));
+	};
+	const getFilteredList = () => {
+		return teachers.filter((item) => {
+			if (filters.active === false && !item.active) return false;
+			
+			return (
+				(filters.name === null || item.teacher_name.toLowerCase().includes(filters.name.toLowerCase())) &&
+				(filters.email === null || item.teacher_email.toLowerCase().includes(filters.email.toLowerCase()))
+			);
+		});
+	};
+
+
   return (
     <div className={`overlay ${show ? 'd-flex justify-content-center align-items-center' : 'd-none'}`}>
       <div className='pop-up col-sm-11 col-md-6 d-flex flex-column gap-4'>
@@ -38,28 +74,29 @@ const CommissionModal = ({show, setShow, area, teachers, comission, setComission
           <Alert text="Nenhum docente encontrado" />
         ) : (
           <div className="view">
-            <table>
+            <table className="w-100">
               <tr className='header'>
-                <th><p>#</p></th>
-                <th><p>Nome</p></th>
-                <th><p>Email</p></th>
                 <th></th>
+								<th><p><input placeholder='Nome do Docente' onChange={(e) => setName(e.target.value)}/></p></th>
+								<th><p><input placeholder='Email' onChange={(e) => setEmail(e.target.value)}/></p></th>
               </tr>
-              {teachers
+              {getFilteredList()
                 .map(e => (
                   <tr className='table-row' key={e.id}>
-                    <th><p>{e.id_teacher}</p></th>
-                    <th><p>{e.teacher_name}</p></th>
-                    <th><p><a href={`mailto:${e.teacher_email}`}>{e.teacher_email}</a></p></th>
-                    <th>
+										<th className="fit-column">
                       <CheckBox
                         value={tempComission.some(t => t.teacher_id === e.id_teacher)}
                         setValue={(checked) => handleToggle(e, checked)}
                       />
                     </th>
+                    <th><p>{e.teacher_name}</p></th>
+                    <th><p><a href={`mailto:${e.teacher_email}`}>{e.teacher_email}</a></p></th>
                   </tr>
                 ))}
             </table>
+
+						{teachers?.length > 0 && getFilteredList().length === 0 && <Alert text='Não foi encontrado nenhuma Docente' />}
+
           </div>
         )}
 
